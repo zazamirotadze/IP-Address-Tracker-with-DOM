98afd1559a8b47eb0b7f6a2b80875b8e350f747d
zaza mirotadze <mirotadzezaza1@gmail.com>
2022-12-15 12:20:40 +0400

    add ip adress tracker with dom manipulation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let ip = ""
 
-
+var box = document.getElementById("map")
 
 
 
@@ -9,6 +9,7 @@ function initialFetch(){
     fetch("https://ipapi.co/json/")
     .then((response) => response.json())
     .then((data) =>{ 
+       
         document.getElementById("ip-data").innerHTML = data.ip
         document.getElementById("location-data").innerHTML = data.city
         document.getElementById("utc-data").innerHTML = data.utc_offset
@@ -31,7 +32,6 @@ function getInputValue(){
     fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_HjOp99VR0QnmxhSYM2SSm69KJ5U7X&ipAddress=${ip}`)
     .then((response) => response.json())
     .then((data) =>{ 
-
         if(data.code===undefined) {
             document.getElementById("ip-data").innerHTML = data.ip
             document.getElementById("location-data").innerHTML = data.location.city
@@ -47,8 +47,13 @@ function getInputValue(){
 
 
 updateMarker = (lat =initialLat, lng = initialLng) => {
-    map = L.map('map').setView([lat, lng], 10,  );
-    var marker = L.marker([lat, lng]).addTo(map);
+    map = L.map('map', { zoomControl: false }).setView([lat, lng], 16,  );
+    var greenIcon = L.icon({
+        iconUrl: './images/icon-location.svg',
+        iconSize:     [38, 48], // size of the icon// size of the shadow
+
+    });
+    var marker = L.marker([lat, lng], {icon: greenIcon}).addTo(map);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         noWrap: true,
         maxZoom: 19,
